f517170a0b4b526ac1fc2c640b796c9c0084adad
Carlos <carloscollado2001@gmail.com>
2021-03-25 16:22:08 -0400

    added main menu

---
+++ Assets/Script/GameManagement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagement : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject FireButton;
+    public GameObject JumpButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Time.timeScale = 0;
+        
+    }
+
+    public void PlayButton()
+    {
+        MainMenu.SetActive(false);
+        FireButton.SetActive(true);
+        JumpButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+    
+}
