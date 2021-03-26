e73afe3cfb0e548fd72f1c6961c843427aade171
Carlos <carloscollado2001@gmail.com>
2021-03-25 20:18:41 -0400

    avance main menu, added pause menu and game over (game over needs fix in retry button)

--- Assets/Script/GameManagement.cs
+++ Assets/Script/GameManagement.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManagement : MonoBehaviour
 {
     public GameObject MainMenu;
-    public GameObject FireButton;
-    public GameObject JumpButton;
+    public GameObject PauseMenu;
+    public GameObject Retry;
+    public GameObject Buttons;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +22,33 @@ public class GameManagement : MonoBehaviour
     public void PlayButton()
     {
         MainMenu.SetActive(false);
-        FireButton.SetActive(true);
-        JumpButton.SetActive(true);
+        Buttons.SetActive(true);
         Time.timeScale = 1;
+    }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+        PauseMenu.SetActive(true);
+    }
+
+    public void RetryButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Retry.SetActive(false);
+        MainMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ResumeButton()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     
 }
--- Assets/Script/ship_behaviour.cs
+++ Assets/Script/ship_behaviour.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class ship_behaviour : MonoBehaviour
 {
+    public GameObject Retry;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Finish")
         {
             Destroy(this.gameObject);
             Time.timeScale= 0;
+            Retry.SetActive(true);
         }
     }
 }
