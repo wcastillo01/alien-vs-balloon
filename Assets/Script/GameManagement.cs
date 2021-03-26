5ced2d30bfecd3cdb4a66d0fcb2471dd50231571
Carlos <carloscollado2001@gmail.com>
2021-03-26 11:46:36 -0400

    fixed timer

--- Assets/Script/GameManagement.cs
+++ Assets/Script/GameManagement.cs
@@ -10,54 +10,41 @@ public class GameManagement : MonoBehaviour
     public GameObject PauseMenu;
     public GameObject Retry;
     public GameObject Buttons;
-    public Text clock;
 
-    private bool timer = false;
+    private Timer time;
+
 
     // Start is called before the first frame update
     void Start()
     {
 
         Time.timeScale = 0;
-        
+
     }
 
-    private void Update()
-    {
-        if(timer)
-        {
-            clock.text = Time.time.ToString("F0");
-        }
-    }
 
     public void PlayButton()
     {
         MainMenu.SetActive(false);
         Buttons.SetActive(true);
         Time.timeScale = 1;
-        timer = true;
     }
 
     public void PauseButton()
     {
         Time.timeScale = 0;
         PauseMenu.SetActive(true);
-        timer = false;
     }
 
     public void RetryButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        Retry.SetActive(false);
-        MainMenu.SetActive(false);
-        Time.timeScale = 1;
     }
 
     public void ResumeButton()
     {
         PauseMenu.SetActive(false);
         Time.timeScale = 1;
-        timer = true;
     }
 
     public void ExitToMenu()
---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public bool timer_on;
+    public Text clock;
+
+    static private float timer;
+
+
+    private void Start()
+    {
+        clock.text = timer.ToString("F0");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        timer += Time.deltaTime;
+        clock.text = timer.ToString("F0");
+        
+    }
+
+    public void ResetTimer()
+    {
+        timer = 0;
+    }
+}
